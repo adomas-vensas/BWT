3d382404364f360f16fd49cbdb2c58daa956b850
adomas-vensas <adomasve@gmail.com>
2025-04-21 12:35:53 +0300

    add getMacroscopic

--- src/simulation/lbm.ts
+++ src/simulation/lbm.ts
@@ -15,6 +15,14 @@ const VELOCITIES = tf.tensor2d([
 
 const WEIGHTS = tf.tensor([4/9, 1/9, 1/9, 1/9, 1/9, 1/36, 1/36, 1/36, 1/36])
 
+
+const RIGHT_DIRS = tf.tensor1d([1, 5, 8], 'int32');
+const LEFT_DIRS  = tf.tensor1d([3, 7, 6], 'int32');
+const UP_DIRS    = tf.tensor1d([2, 5, 6], 'int32');
+const DOWN_DIRS  = tf.tensor1d([4, 7, 8], 'int32');
+const ALL_DIRS   = tf.tensor1d([0, 1, 2, 3, 4, 5, 6, 7, 8], 'int32');
+const OPP_DIRS   = tf.tensor1d([0, 3, 4, 1, 2, 7, 8, 5, 6], 'int32');
+
 /**
  * Compute the equilibrium distribution function feq.
  * 
@@ -57,3 +65,33 @@ export function getEquilibrium(rho: tf.Tensor2D, u: tf.Tensor3D): tf.Tensor3D {
     return feq as tf.Tensor3D;
   });
 }
+
+/**
+ * Calculate the macroscopic density and velocity from the distribution function.
+ * @param f tf.Tensor3D of shape [9, NX, NY] - distribution function
+ * @returns { rho: tf.Tensor2D, u: tf.Tensor3D [2, NX, NY] } - macroscopic properties
+ */
+export function getMacroscopic(
+    f: tf.Tensor3D
+  ): { rho: tf.Tensor2D, u: tf.Tensor3D } {
+    return tf.tidy(() => {
+      // Density: sum over all directions (axis 0)
+      const rho = tf.sum(f, 0) as tf.Tensor2D; // shape [NX, NY]
+  
+      // Velocity components
+      const u_x = tf.divNoNan(
+        tf.sub(tf.sum(tf.gather(f, RIGHT_DIRS), 0), tf.sum(tf.gather(f, LEFT_DIRS), 0)),
+        rho
+      ); // [NX, NY]
+  
+      const u_y = tf.divNoNan(
+        tf.sub(tf.sum(tf.gather(f, UP_DIRS), 0), tf.sum(tf.gather(f, DOWN_DIRS), 0)),
+        rho
+      ); // [NX, NY]
+  
+      // Stack into velocity tensor [2, NX, NY]
+      const u = tf.stack([u_x, u_y], 0); // shape [2, NX, NY]
+  
+      return { rho, u: u as tf.Tensor3D };
+    });
+  }
