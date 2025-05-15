380cb4ec7f2d49c7c78468315cd16d68135dd0e0
adomas-vensas <adomasve@gmail.com>
2025-05-15 19:02:30 +0300

    fix modal panel header

--- src/Panels/About/AboutPanelModal.tsx
+++ src/Panels/About/AboutPanelModal.tsx
@@ -19,7 +19,7 @@ export default function AboutPanelModal({ open }: AboutPanelModalProps) {
               {/* Modal header */}
               <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
                 <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                  Real Time Parameters
+                  About the Project
                 </h3>
               </div>
 
